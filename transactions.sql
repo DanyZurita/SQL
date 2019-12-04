ef897c7fe03c84e59ddfb83b06b05d26b8b6e04f
DanyZurita <dzurita@cifpfbmoll.eu>
2019-12-04 17:08:56 +0100

    feat(transactions): Ejercicio 18 de 18 acabado

--- transactions.sql
+++ transactions.sql
@@ -8,65 +8,74 @@
 
 /* 3. */
 
-    
+    SET autocommit = 0
+    /* o */
+    SET autocommit = OFF
+    /* and */
+    START TRANSACTION;
 
 /* 4. */
 
-    
+    SELECT AVG(E.comission) FROM EMPLOYEES AS E
 
 /* 5. */
 
-    
+    UPDATE EMPLOYEES SET comission = comission * 1.20
 
 /* 6. */
 
-    
+    SELECT AVG(E.comission) FROM EMPLOYEES AS E
+    /* Yes. */
 
 /* 7. */
 
-    
+    DELETE FROM EMPLOYEES WHERE surname = "TOVAR"
 
 /* 8. */
 
-    
+    ROLLBACK
 
 /* 9. */
 
-    
+    SELECT AVG(E.comission) FROM EMPLOYEES AS E
+    SELECT * FROM EMPLOYEES WHERE surname = "TOVAR"
 
 /* 10. */
 
-    
+    SAVEPOINT SPINCCOMMISSION
 
 /* 11. */
 
-    
+    UPDATE EMPLOYEES SET comission = comission * 1.20
 
 /* 12. */
 
-    
+    SELECT AVG(E.comission) FROM EMPLOYEES AS E
+    /* Yes. */
 
 /* 13. */
 
-    
+    SAVEPOINT SPDELTOVAR
 
 /* 14. */
 
-    
+    DELETE FROM EMPLOYEES WHERE surname = "TOVAR"
 
 /* 15. */
 
-    
-
+    ROLLBACK TO SAVEPOINT SPDELTOVAR
 
 /* 16. */
 
-    
+    SELECT AVG(E.comission) FROM EMPLOYEES AS E
+    SELECT * FROM EMPLOYEES WHERE surname = "TOVAR"
 
 /* 17. */
 
-    
+    COMMIT
 
 /* 18. */
 
-    
+    SELECT AVG(E.comission) FROM EMPLOYEES AS E
+    SELECT * FROM EMPLOYEES WHERE surname = "TOVAR"
+    /* The 20% raise of commission is implemented and the employee TOVAR is in the DB */
