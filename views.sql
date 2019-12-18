6bddf110528c3252dcaeffd6637151cc4cd6ada8
DanyZurita <dzurita@cifpfbmoll.eu>
2019-12-18 08:33:03 +0100

    feat(views): Ejercicio 3 de 7 acabado

--- views.sql
+++ views.sql
@@ -1,26 +1,28 @@
 /* 1. */
 
-CREATE VIEW V_DEPARTMENTS AS
-    SELECT D.num, D.name, D.town_code, T.name AS town_name, COUNT(E.num) AS num_employees 
-    FROM DEPARTMENTS AS D LEFT OUTER JOIN TOWNS AS T ON D.town_code = T.code 
-                        LEFT OUTER JOIN EMPLOYEES AS E ON D.num = E.dept_num 
-                        GROUP BY D.num;
+    CREATE VIEW V_DEPARTMENTS AS
+        SELECT D.num, D.name, D.town_code, T.name AS town_name, COUNT(E.num) AS num_employees 
+        FROM DEPARTMENTS AS D LEFT OUTER JOIN TOWNS AS T ON D.town_code = T.code 
+                            LEFT OUTER JOIN EMPLOYEES AS E ON D.num = E.dept_num 
+                            GROUP BY D.num;
 
 /* 2. */
 
-ALTER VIEW V_DEPARTMENTS AS
-    SELECT D.num, D.name, D.town_code, T.name AS town_name, COUNT(E.num) AS num_employees, AVG(E.salary) AS average_salary
-    FROM DEPARTMENTS AS D LEFT OUTER JOIN TOWNS AS T ON D.town_code = T.code 
-                        LEFT OUTER JOIN EMPLOYEES AS E ON D.num = E.dept_num 
-                        GROUP BY D.num;
+    ALTER VIEW V_DEPARTMENTS AS
+        SELECT D.num, D.name, D.town_code, T.name AS town_name, COUNT(E.num) AS num_employees, AVG(E.salary) AS average_salary
+        FROM DEPARTMENTS AS D LEFT OUTER JOIN TOWNS AS T ON D.town_code = T.code 
+                            LEFT OUTER JOIN EMPLOYEES AS E ON D.num = E.dept_num 
+                            GROUP BY D.num;
 
 /* 3. */
 
-    
+   INSERT INTO DEPARTMENTS VALUES (60, 'Human Resources', 'MAD');
+   INSERT INTO EMPLOYEES VALUES (9999, 'GONZALEZ', 'SERGI', NULL, '2019-01-01', 9000, NULL, 60, NULL);
+   SELECT * FROM V_DEPARTMENTS;
 
 /* 4. */
 
-
+    
 
 /* 5. */
 
