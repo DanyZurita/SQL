aafb3c05dae5d150dac1129142f94bbaf615c7cd
DanyZurita <dzurita@cifpfbmoll.eu>
2019-12-18 08:29:20 +0100

    feat(views): Ejercicio 2 de 7 acabado

--- views.sql
+++ views.sql
@@ -1,10 +1,18 @@
 /* 1. */
 
-
+CREATE VIEW V_DEPARTMENTS AS
+    SELECT D.num, D.name, D.town_code, T.name AS town_name, COUNT(E.num) AS num_employees 
+    FROM DEPARTMENTS AS D LEFT OUTER JOIN TOWNS AS T ON D.town_code = T.code 
+                        LEFT OUTER JOIN EMPLOYEES AS E ON D.num = E.dept_num 
+                        GROUP BY D.num;
 
 /* 2. */
 
-
+ALTER VIEW V_DEPARTMENTS AS
+    SELECT D.num, D.name, D.town_code, T.name AS town_name, COUNT(E.num) AS num_employees, AVG(E.salary) AS average_salary
+    FROM DEPARTMENTS AS D LEFT OUTER JOIN TOWNS AS T ON D.town_code = T.code 
+                        LEFT OUTER JOIN EMPLOYEES AS E ON D.num = E.dept_num 
+                        GROUP BY D.num;
 
 /* 3. */
 
