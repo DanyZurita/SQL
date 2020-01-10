fbcf778409ede057a22e6a554aafe67eb30f8b66
DanyZurita <dzurita@cifpfbmoll.eu>
2020-01-10 20:49:50 +0100

    feat(tennis): Ejercicio 1 de 17 acabado

--- P04tennis.sql
+++ P04tennis.sql
@@ -1,6 +1,9 @@
 /* 1. */
 
-    
+    SELECT C.c_name AS CountryName, COUNT(P.p_id) AS NumberOfPlayers
+    FROM COUNTRIES AS C, PLAYERS AS P WHERE C.c_id = P.p_c_id
+        GROUP BY C.c_name
+        ORDER BY C.c_name;
 
 /* 2. */
 
