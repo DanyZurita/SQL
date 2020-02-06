3cf5b0eea1a52b15b200ede5d2c3f604ccbe8a7c
DanyZurita <dzurita@cifpfbmoll.eu>
2020-02-06 20:25:07 +0100

    feat(P07): Acabar ejercicio 3 de 47

---
+++ P07.sql
@@ -0,0 +1,71 @@
+/* 0. */
+
+    /* looking for a hotel room, more or less */
+
+/* 1. */
+
+    /* Drawing of the entity relational model */
+
+/* 2. */
+
+    /* Drawing of the entity relational model */
+
+/* 3. */
+
+    /* postgres@DBVMPC:~$ createdb bookings -O booking */
+
+/* 4. */
+
+    
+
+/* 5. */
+
+    
+
+/* 6. */
+
+    
+
+/* 7. */          
+
+
+
+/* 8. */
+
+    
+
+/* 9. */
+
+    
+
+/* 10. */
+
+    
+
+/* 11. */
+
+    
+
+/* 12. */
+
+    
+
+/* 13. */
+
+    
+
+/* 14. */
+
+    
+
+/* 15. */
+
+    
+
+/* 16. */
+
+    
+
+/* 17. */
+
+            
