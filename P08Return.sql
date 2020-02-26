56310707a5ee188fbe0bd0142c9f02d822d5bef5
DanyZurita <dzurita@cifpfbmoll.eu>
2020-02-26 18:32:23 +0100

    feat(P08Return): Acabar ejercicio 2 de 13

--- P08Return.sql
+++ P08Return.sql
@@ -30,7 +30,20 @@
 
 /* 2. */
 
-    
+    INSERT INTO data.customers VALUES (20001, 'Sergi', 'González', '6224597470 Dell Way', NULL, 'DVCINXG', NULL, 0, 'Australia', 2, 'sg@dell.com', '6224597470', 3, '1869697669055999', '2010/07', 'user20001', 'password', 17, 40000, 'F');
+    INSERT INTO data.customers VALUES (20002, 'Pep', 'López', '6224597470 Dell Way', NULL, 'DVCINXG', NULL, 0, 'Australia', 2, 'sg@dell.com', '6224597470', 3, '1869697669055999', '2010/07', 'user20002', 'password', 17, 40000, 'F');
+
+    create or replace function del_under18_2()
+    returns record AS $$
+        DECLARE
+            before int := (select count(*) from customers);
+            after int = 0;
+        BEGIN
+            DELETE FROM customers where age < 18;
+            after := (select count(*) from customers);
+            return (before, after);
+        END
+    $$ LANGUAGE plpgsql;
 
 /* 3. */
 
