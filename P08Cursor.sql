ac383a01739735d27661a0f6243fc17d2ff8eb2e
DanyZurita <dzurita@cifpfbmoll.eu>
2020-03-11 17:54:33 +0100

    feat(P08Cursor): Acabar ejercicio 1 de 5

---
+++ P08Cursor.sql
@@ -0,0 +1,40 @@
+/* 0. */
+
+    /* create database employeesdb */
+    /* \c employeesdb */
+    /* \i /mnt/c/Users/dajoz/Downloads/EMPLOYEESDB.sql */
+
+/* 1. */
+
+    DO $$
+    DECLARE
+        Nums integer;
+        Names text;
+        Surnames text;
+        Dept_nums integer;
+        empCursor CURSOR for SELECT num, name, surname, dept_num from employees order by surname, name;
+    BEGIN
+        OPEN empCursor;
+        fetch empCursor into Nums, Names, Surnames, Dept_nums;
+        raise notice '% % % %', Nums, Names, Surnames, Dept_nums;
+        fetch last from empCursor into Nums, Names, Surnames, Dept_nums;
+        raise notice '% % % %', Nums, Names, Surnames, Dept_nums;
+        CLOSE empCursor;
+    END
+    $$ language plpgsql;
+
+/* 2. */
+
+    
+
+/* 3. */
+
+    
+
+/* 4. */
+
+    
+
+/* 5. */
+
+    
