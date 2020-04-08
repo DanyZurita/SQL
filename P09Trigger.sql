6d5e7bb9216f9ec8fd0893234923a15f6e034cf6
DanyZurita <dzurita@cifpfbmoll.eu>
2020-04-08 18:57:16 +0200

    feat(P09Triggers): Acabar ejercicio 1 de 11

--- P09Trigger.sql
+++ P09Trigger.sql
@@ -6,7 +6,28 @@
 
 /* 1. */
 
-    
+    /* Name to assign to the trigger: insert_mondays */
+    /* Name to assign to the function runned by the TRIGGER: if_monday() */
+    /* Table on what the trigger will ‘jump’ when an operation is performed: employees */
+    /* When the trigger will be executed (BEFORE or AFTER): BEFORE */
+    /* Operation that will activate the trigger: INSERT */
+    /* The execution of the trigger will be executed for each row (several times) or only when the sentence is executed (one time): Executed for every row inserted */
+
+    CREATE or replace FUNCTION if_monday() RETURNS TRIGGER AS
+    $$
+    BEGIN
+        IF (to_char(NOW(), 'Dy') != 'Mon') 
+            THEN RAISE EXCEPTION 'INSERT EMPLOYEE MONDAY'
+                USING HINT = 'You can only insert employees on Monday!';
+        END IF;
+        RETURN NULL;
+    END;
+    $$ language plpgsql;
+
+    CREATE TRIGGER insert_mondays
+        BEFORE INSERT ON employees 
+            FOR EACH STATEMENT EXECUTE PROCEDURE if_monday();
+
 
 /* 2. */
 
