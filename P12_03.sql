90f5a4371722e00d68e8d4518e722f8f1e59c7b2
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-11 17:56:17 +0200

    style(P12_03): Añadir comentarios con el enunciado de los ejercicios

--- P12_03.sql
+++ P12_03.sql
@@ -1,79 +1,20 @@
-/* 1. */
+/* 1. Insert multiple companies (company data of your choice) 
+into a collection - both with insertOne() and insertMany() */
 
     
 
-/* 2. */
+/* 2. Deliberately insert duplicate ID data and "fix" failing 
+additions with unordered inserts. */
 
     
 
-/* 3. */
+/* 3. Write data for a new company with both journaling being 
+guaranteed and not being guaranteed. */
 
     
 
-/* 4. */
+/* 4. Insert this json file inside a database named “tvshowsdb” 
+and into a collection named “tvshows”. To to this use mongoimport. */
 
     
-
-/* 5. */
-
-    
-
-/* 6. */
-
-    
-
-/* 7. */          
-
-
-
-/* 8. */
-
-    
-
-/* 9. */
-
-    
-
-/* 10. */
-
-    
-
-/* 11. */
-
-    
-
-/* 12. */
-
-    
-
-/* 13. */
-
-    
-
-/* 14. */
-
-    
-
-/* 15. */
-
-    
-
-/* 16. */
-
-    
-
-/* 17. */
-
-            
-
-/* 18. */
-
-    
-
-/* 19. */
-
-    
-
-/* 20. */
-
 
