eb467519c6a6931060f6c3a936233e487a98f3ef
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-15 23:41:05 +0200

    feat(P12_07): Añadir plantilla con comentarios de ejercicios de P12_07

---
+++ P12_07.sql
@@ -0,0 +1,21 @@
+/* 1. Total number of persons per gender. Sort the result by amount of person per gender desc. 
+Execution example: 
+{"_id": {"gender": "female"}, "personsPerGender": 2565}
+{"_id": {"gender": "male"}, "personsPerGender": 2435} */
+
+    
+
+/* 2. Total number of persons per gender but with age greater then 50. Sort the result by amount 
+of person per gender desc. 
+Execution example: 
+{"_id": {"gender": "female"}, "personsPerGender": 1125}
+{"_id": {"gender": "male"}, "personsPerGender": 1079} */
+
+    
+
+/* 3. Total number of persons per gender but with age greater then 50 and now also show the average age of the two groups. Sort the result by amount of person per gender desc. 
+Execution example: 
+{"_id": {"gender": "female"}, "personsPerGender": 1125, "averageAge": 61.90577777777778}
+{"_id": {"gender": "male"}, "personsPerGender": 1079, "averageAge": 62.0667284} */
+
+    
