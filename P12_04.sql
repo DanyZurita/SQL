6e5d83acc8f3d2fc97cf20455ce2e6d20c1f339a
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-15 21:17:06 +0200

    feat(P12_04): Añadir plantilla con comentarios de ejercicios de P12_04

---
+++ P12_04.sql
@@ -0,0 +1,17 @@
+/* 1. Import this json file data into a new database (e.g. boxOffice) 
+and collection (e.g. movieStarts). */
+
+    
+
+/* 2. Search all movies that have a rating higher than 9.2 and a 
+runtime lower than 100 minutes. */
+
+    
+
+/* 3. Search all movies that have a genre of “drama” or “action”. */
+
+    
+
+/* 4. Search all movies where field “runtime” exceeded field “weight”. */
+
+    
