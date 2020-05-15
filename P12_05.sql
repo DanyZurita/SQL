823a1822e03947862ec18663af4ea951bee62b21
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-15 22:27:18 +0200

    feat(P12_05): Añadir plantilla con comentarios de ejercicios de P12_05

---
+++ P12_05.sql
@@ -0,0 +1,21 @@
+/* 1. Import this json file data into a new database (e.g. boxOffice) 
+and collection (e.g. movieStarts). */
+
+    
+
+/* 2. Find all movies with exactly two genres. */
+
+    
+
+/* 3. Find all movies with scheduled time 21:00. */
+
+    
+
+/* 4. Find all movies with scheduled time 21:00 and days Sunday. */
+
+    
+
+/* 5. Find all movies with genres “Action”, “Drama” and “Thriller” (only those ones and in any order). 
+Use $all. */
+
+    
