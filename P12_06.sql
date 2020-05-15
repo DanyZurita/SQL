a2182e0c07eef1d10a7b977fd8f63561551f2ff7
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-15 23:07:51 +0200

    feat(P12_06): Acabar ejercicio 1 de 3

--- P12_06.sql
+++ P12_06.sql
@@ -1,7 +1,16 @@
 /* 1. Create a new collection ("sports") and upsert two new documents into it
 (with these fields: "title"-string, "requiresTeam"-boolean) */
 
-    
+    use sportsdb
+
+    db.sports.insertMany([{
+        title: "team1",
+        requiresTeam: true
+    },
+    {
+        title: "team2",
+        requiresTeam: false
+    }])
 
 /* 2. Update all documents which do require a tem by addin a new field with the minimum
 number of player requiered. Use upsert. */
@@ -11,4 +20,4 @@ number of player requiered. Use upsert. */
 /* 3. Update all documents that require a team by increasing the number of required players
 by 10. Use upsert. */
 
-
+    
