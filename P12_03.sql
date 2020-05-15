b129b88b8f1a1468c82909300055e7612770fe27
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-15 21:33:04 +0200

    feat(P12_03): Acabar ejercicio 1 de 4

--- P12_03.sql
+++ P12_03.sql
@@ -1,12 +1,26 @@
 /* 1. Insert multiple companies (company data of your choice) 
 into a collection - both with insertOne() and insertMany() */
 
-    
+    use test
+
+    db.companies.insertMany([{
+        _id: "000001",
+        name: "Dany Zurita SL",
+        dateOfFoundation: "07/04/2019",
+        owner: "Dany Zurita",
+        email: "dzurita@cifpfbmoll.eu"
+    },
+    {
+        _id: "000002",
+        name: "Isidre Marti SL",
+        dateOfFoundation: "22/12/2019",
+        owner: "Isidre Martí",
+        email: "imarti@cifpfbmoll.eu"
+    }])
 
 /* 2. Deliberately insert duplicate ID data and "fix" failing 
 additions with unordered inserts. */
 
-    
 
 /* 3. Write data for a new company with both journaling being 
 guaranteed and not being guaranteed. */
@@ -17,4 +31,3 @@ guaranteed and not being guaranteed. */
 and into a collection named “tvshows”. To to this use mongoimport. */
 
     
-
