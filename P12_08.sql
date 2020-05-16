b23e3518d71aa27e479ec5188c0b9a91b77d85ad
DanyZurita <dzurita@cifpfbmoll.eu>
2020-05-17 01:30:10 +0200

    feat(P12_08): Añadir plantilla con comentarios de ejercicios de P12_08

---
+++ P12_08.sql
@@ -0,0 +1,24 @@
+/* Create 4 users: */
+
+/* 1. A DBA user (name "userAdmin"). */
+
+    
+
+/* 2. Database Admin (name "userDbAdmin"): Work on all databases, create collections, create indexes, 
+etc. But it's not allowed to create users… */
+
+    
+
+/* 3. User Admin (name "userAdm"): Allowed to manage users. */
+
+    
+
+/* 4. Developer (name "userDev"): Read and write data in "customers" and "sales" databases. */
+
+    
+
+/* 5. Finally, show all the created users inside the database "admin" and "customers". */
+
+    
+
+/* Obviously, you must check the built-in roles… */
