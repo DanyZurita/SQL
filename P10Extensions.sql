eb13b583822efc185ad4345a168ff4055dc1983e
DanyZurita <dzurita@cifpfbmoll.eu>
2020-06-04 14:45:27 +0200

    feat(P10Extensions): Acabar ejercicio 1 de 2

--- P10Extensions.sql
+++ P10Extensions.sql
@@ -1,17 +1,19 @@
 /* 0. Create the following table:
 
-CREATE TABLE highschools (
-  code VARCHAR(3) NOT NULL PRIMARY KEY,
-  geog GEOGRAPHY(Point)
-);
-*/
-
-    
+    CREATE TABLE highschools (
+    code VARCHAR(3) NOT NULL PRIMARY KEY,
+    geog GEOGRAPHY(Point)
+    );
+*/    
 
 /* 1. If you tried to run that create table you got an error. Explain why and solve it. */
 
-    /* */
+    /* Becuase the 'geography' type does not exist yet, you have to install postGIS for postgres 12 
+    with the command:  
+        $ sudo apt-get install postgis 
+    and create the postGIS extension in postgres with:
+        CREATE EXTENSION postgis; */
 
 /* 2. Explain what’s a GEOGRAPHY(point) data type. */
 
-    /* */
+    /*  */
