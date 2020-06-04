edaf8c3427cbebd00cffd93960b39e83f5844552
DanyZurita <dzurita@cifpfbmoll.eu>
2020-06-04 13:45:45 +0200

    feat(P10Extensions): Añadir plantilla enunciado ejercicio P10Extensions

---
+++ P10Extensions.sql
@@ -0,0 +1,17 @@
+/* 0. Create the following table:
+
+CREATE TABLE highschools (
+  code VARCHAR(3) NOT NULL PRIMARY KEY,
+  geog GEOGRAPHY(Point)
+);
+*/
+
+    
+
+/* 1. If you tried to run that create table you got an error. Explain why and solve it. */
+
+    /* */
+
+/* 2. Explain what’s a GEOGRAPHY(point) data type. */
+
+    /* */
