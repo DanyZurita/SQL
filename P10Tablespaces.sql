cd3f0aaeb421822a7f6edf04053ba78cdb2c716e
DanyZurita <dzurita@cifpfbmoll.eu>
2020-06-05 16:41:26 +0200

    feat(P10Tablespaces): Acabar ejercicio 1 de 2

--- P10Tablespaces.sql
+++ P10Tablespaces.sql
@@ -25,6 +25,48 @@ Clue: To list all indexes of the schema “public” run:
     Clue: Do a LOOP with ALTER INDEX…
  */
 
+    /*  $ sudo apt install unzip
+        $ sudo unzip /mnt/c/Users/dajoz/Downloads/dvdrental.zip
+        CREATE DATABASE dvdrental;
+        $ sudo su postgres
+        $ pg_restore -U postgres -d dvdrental dvdrental.tar
+    */
+    
+    /*  $ sudo mkdir /indexes/dvdrental
+        $ sudo mkdir /tables/dvdrental
+    
+        $ sudo chown -R postgres:postgres /tables/dvdrental
+        $ sudo chown -R postgres:postgres /indexes/dvdrental
+
+    */
+
+    CREATE TABLESPACE dvdrental_t
+    OWNER postgres
+    LOCATION '/tables/dvdrental';
+
+    CREATE TABLESPACE dvdrental_i
+    OWNER postgres
+    LOCATION '/indexes/dvdrental';
+
+    DO $$
+    DECLARE 
+        rec RECORD;
+    BEGIN
+        FOR rec IN SELECT
+                indexname,
+                tablespace
+            FROM
+                pg_indexes
+            WHERE
+                schemaname = 'public'
+            ORDER BY
+                tablename,
+                indexname LOOP
+        EXECUTE FORMAT('ALTER INDEX %I SET TABLESPACE dvdrental_i;', rec.indexname);
+        RAISE NOTICE 'ALTER INDEX % SET TABLESPACE dvdrental_i;', rec.indexname;
+        END LOOP;
+    END $$;
+
 /* 2. Places all the tables of the database “dvdrental” inside the directory “/tables/dvdrental”.
 Clue: To list all tables of the schema “public” run:
     select *
